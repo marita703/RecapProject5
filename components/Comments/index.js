d2781f463e5f3e65dae2476ac65cccec09a63df2
Tori Luszczek <wuszczek@gmail.com>
2023-04-27 12:10:59 +0200

    so the comments are adding, but they are adding twice for whatever reason

--- components/Comments/index.js
+++ components/Comments/index.js
@@ -1,16 +1,20 @@
 // This will render a list of the comments (createElement)
 import { useEffect, useState } from "react";
+import { uid } from "uid";
 
 export function Comments(coms = "a thing") {
   const [something, setThing] = useState([]);
 
   something.push(coms);
-  console.log(something);
 
   return (
     <>
       <h1>this is a comment section</h1>
-      <p></p>
+      <p>
+        {something.map((element) => {
+          return <p key={uid}>{element.coms}</p>;
+        })}
+      </p>
     </>
   );
 }
