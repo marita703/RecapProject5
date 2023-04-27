257bfe67b6df9747e2b2eabc91218e282c6b8ea5
Tori Luszczek <wuszczek@gmail.com>
2023-04-27 14:05:45 +0200

    navigation links added.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { ArtPiecePreview } from "../components/ArtPiecePreview";
 import { ArtPieces } from "../components/ArtPieces";
 import { SpotLight } from "../components/SpotLight";
+import { Navigation } from "../components/Navigation";
 export default function HomePage() {
   return (
     <div>
       <SpotLight />
       <ArtPieces />
+      <Navigation />
     </div>
   );
 }
