8d300d80904cc41ef841162c615dc8dd0ce9e9b6
Maria del Mar <chocoinformatics2016@gmail.com>
2023-04-27 11:22:37 +0200

    changed the fetch to app, and finished with random component and art pieces

--- components/SpotLight/index.js
+++ components/SpotLight/index.js
@@ -1,5 +1,37 @@
 // This should render a random Picture that accepts image and artist as props
 
-export function SpotLight({ image, artist }) {
-  return;
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+import { useArtStore } from "../../stores/artpieces";
+
+export function SpotLight() {
+  const { artPiecesInfo } = useArtStore();
+  console.log("SPOTLIGHT: ", artPiecesInfo);
+
+  const [randomObject, setRandomObject] = useState({});
+
+  const getRandomObject = (array) => {
+    const randomObject = array[Math.floor(Math.random() * array.length)];
+    setRandomObject(randomObject);
+  };
+
+  useEffect(() => {
+    getRandomObject(artPiecesInfo);
+  }, [randomObject, artPiecesInfo]);
+
+  console.log(randomObject);
+
+  return (
+    <>
+      <section>
+        <Image
+          src={randomObject.imageSource}
+          alt="Art piece image"
+          width={200}
+          height={200}
+        />
+      </section>
+    </>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
+import { useArtStore } from "../stores/artpieces";
+import { useEffect } from "react";
+import useSWR from "swr";
+
 const fetcher = (...args) => {
   console.log("Fetching Data");
   return fetch(...args).then((res) => res.json());
 };
 
 export default function App({ Component, pageProps }) {
+  const url = "https://example-apis.vercel.app/api/art";
+
+  const { setArtPiecesInfo } = useArtStore();
+
+  const { data, error, isLoading } = useSWR(url, fetcher);
+
+  useEffect(() => {
+    setArtPiecesInfo(data);
+  }, [data, setArtPiecesInfo]);
+
+  if (error) return <div>Failed to Load</div>;
+  if (isLoading) return <div>Is loading</div>;
+
+  console.log("data: ", data);
+
   return (
     <>
       <GlobalStyle />
